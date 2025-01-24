a9d097cab4bb159a9437f9ecb08f41eaf3854c87
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2025-01-24 13:07:37 +0900

    siplify the project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 export default {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/roboguard.github.io' : '',
+  basePath: '/roboguard.github.io',
   images: {
     unoptimized: true,
   },
