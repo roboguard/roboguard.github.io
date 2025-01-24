1516e79e4def120a563b331a288341c3f41b3825
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2025-01-24 13:17:37 +0900

    remove readme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
